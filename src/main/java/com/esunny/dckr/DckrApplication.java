63ebbd2cab8f8df7f64e849a26c42bd6f64e1a06
LGSKOKO <420422844@qq.com>
2021-04-21 22:48:47 +0800

    feat(DckrApplication):Dckr_Service项目启动类

---
+++ src/main/java/com/esunny/dckr/DckrApplication.java
@@ -0,0 +1,13 @@
+package com.esunny.dckr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DckrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DckrApplication.class, args);
+	}
+
+}
